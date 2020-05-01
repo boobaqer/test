f73facc15ffa98e83b4bce11295dccfc06f2a9d2
Ali Mohammad Baqer <boobaqer@icloud.com>
2020-05-01 05:38:06 +0300

    hh

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding()
     }
 }
